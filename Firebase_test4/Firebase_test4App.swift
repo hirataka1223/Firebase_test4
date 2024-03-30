d447eb62e119d920d3325bbb2cfe28fa8d71883e
hirataka1223 <154341912+hirataka1223@users.noreply.github.com>
2024-03-30 12:33:19 +0900

    Initial Commit

---
+++ Firebase_test4/Firebase_test4App.swift
@@ -0,0 +1,17 @@
+//
+//  Firebase_test4App.swift
+//  Firebase_test4
+//
+//  Created by Taka on 2024/03/30.
+//
+
+import SwiftUI
+
+@main
+struct Firebase_test4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
